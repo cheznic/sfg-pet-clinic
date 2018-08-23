f3aae5588c211c34fe19b0d6ece1002ed0658286
Charles Nicoletti <cnicoletti@gmail.com>
2018-08-23 16:08:36 -0700

    List all owners on the Owners index page and all vets on vets index page closes #15 and closes #16

--- pet-clinic-web/src/main/java/me/cheznic/learning/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/me/cheznic/learning/sfgpetclinic/controllers/OwnerController.java
@@ -1,16 +1,28 @@
 package me.cheznic.learning.sfgpetclinic.controllers;
 
+import me.cheznic.learning.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Charles Nicoletti on 8/23/18
  */
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owners", "/owners/", "/owners/index", "/owners/index.html"})
-    public String index(){
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String index(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 
--- pet-clinic-web/src/main/java/me/cheznic/learning/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/me/cheznic/learning/sfgpetclinic/controllers/VetController.java
@@ -1,16 +1,28 @@
 package me.cheznic.learning.sfgpetclinic.controllers;
 
+import me.cheznic.learning.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Charles Nicoletti on 8/23/18
  */
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html"})
-    public String index(){
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String index(Model model)
+    {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 
