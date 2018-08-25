302a3f6da8cde335f589639959d987b837574bb7
Charles Nicoletti <cnicoletti@gmail.com>
2018-08-24 18:10:04 -0700

    create vet speciality entity associate it to vet closes #27

--- pet-clinic-data/src/main/java/me/cheznic/learning/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/me/cheznic/learning/sfgpetclinic/model/Vet.java
@@ -1,9 +1,19 @@
 package me.cheznic.learning.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Charles Nicoletti on 8/21/18
  */
 public class Vet extends Person {
 
+    private Set<VetSpeciality> vetSpecialities;
 
+    public Set<VetSpeciality> getVetSpecialities() {
+        return vetSpecialities;
+    }
+
+    public void setVetSpecialities(Set<VetSpeciality> vetSpecialities) {
+        this.vetSpecialities = vetSpecialities;
+    }
 }
---
+++ pet-clinic-data/src/main/java/me/cheznic/learning/sfgpetclinic/model/VetSpeciality.java
@@ -0,0 +1,17 @@
+package me.cheznic.learning.sfgpetclinic.model;
+
+/**
+ * Created by Charles Nicoletti on 8/24/18
+ */
+public class VetSpeciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
