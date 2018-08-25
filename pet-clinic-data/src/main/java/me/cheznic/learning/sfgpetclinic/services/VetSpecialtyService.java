a3ff36253290dc09a57e4817c964155b3ea27521
Charles Nicoletti <cnicoletti@gmail.com>
2018-08-24 22:48:57 -0700

    created specialty map service impl closes #31

---
+++ pet-clinic-data/src/main/java/me/cheznic/learning/sfgpetclinic/services/VetSpecialtyService.java
@@ -0,0 +1,6 @@
+package me.cheznic.learning.sfgpetclinic.services;
+
+import me.cheznic.learning.sfgpetclinic.model.VetSpeciality;
+
+public interface VetSpecialtyService extends CrudService<VetSpeciality> {
+}
---
+++ pet-clinic-data/src/main/java/me/cheznic/learning/sfgpetclinic/services/map/VetSpecialtyMapService.java
@@ -0,0 +1,36 @@
+package me.cheznic.learning.sfgpetclinic.services.map;
+
+import me.cheznic.learning.sfgpetclinic.model.VetSpeciality;
+import me.cheznic.learning.sfgpetclinic.services.VetSpecialtyService;
+
+import java.util.Set;
+
+/**
+ * Created by Charles Nicoletti on 8/24/18
+ */
+public class VetSpecialtyMapService extends AbstractMapService<VetSpeciality> implements VetSpecialtyService {
+    @Override
+    public Set<VetSpeciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public VetSpeciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(VetSpeciality vetSpeciality) {
+        super.delete(vetSpeciality);
+    }
+
+    @Override
+    public VetSpeciality save(VetSpeciality vetSpeciality) {
+        return super.save(vetSpeciality);
+    }
+}
