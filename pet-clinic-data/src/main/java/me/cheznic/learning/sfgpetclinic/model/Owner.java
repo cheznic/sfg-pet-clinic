c4ed3759db93fd1c0986a98439dfdf3e18129149
Charles Nicoletti <cnicoletti@gmail.com>
2018-08-24 18:00:26 -0700

    created pet type, pet, visit entities closes #26

--- pet-clinic-data/src/main/java/me/cheznic/learning/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/me/cheznic/learning/sfgpetclinic/model/Owner.java
@@ -1,9 +1,19 @@
 package me.cheznic.learning.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Charles Nicoletti on 8/21/18
  */
 public class Owner extends Person {
 
+    private Set<Pet> pets;
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
