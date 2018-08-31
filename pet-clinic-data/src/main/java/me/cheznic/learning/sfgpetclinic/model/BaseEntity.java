46868817e6278dbba66b389ff5b56973762cd54d
Charles Nicoletti <cnicoletti@gmail.com>
2018-08-30 17:12:42 -0700

    implemented base JPA entity closes #35

--- pet-clinic-data/src/main/java/me/cheznic/learning/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/me/cheznic/learning/sfgpetclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package me.cheznic.learning.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by Charles Nicoletti on 8/22/18
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
