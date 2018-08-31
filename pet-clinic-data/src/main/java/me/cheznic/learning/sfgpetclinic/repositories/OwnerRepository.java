94df6f245105864e44ce3388a394a62bdd0fe3f0
Charles Nicoletti <cnicoletti@gmail.com>
2018-08-30 21:40:32 -0700

    created spring data jpa service for owners. closes #40

--- pet-clinic-data/src/main/java/me/cheznic/learning/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/me/cheznic/learning/sfgpetclinic/repositories/OwnerRepository.java
@@ -2,11 +2,23 @@ package me.cheznic.learning.sfgpetclinic.repositories;
 
 import me.cheznic.learning.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 /**
  * Created by Charles Nicoletti on 8/30/18
  */
-@Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    public Owner findByLastName(String lastName);
+
+    public Set<Owner> findAllBy();
+
+    public Owner findById();
+
+    public Owner save(Owner owner);
+
+    public void delete(Owner owner);
+
+    public void deleteById(Long id);
 }
