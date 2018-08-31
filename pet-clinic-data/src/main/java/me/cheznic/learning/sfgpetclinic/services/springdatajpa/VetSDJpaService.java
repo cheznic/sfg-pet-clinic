5126fb98fdea6e2986c413e8416fe5b5ba166a12
Charles Nicoletti <cnicoletti@gmail.com>
2018-08-30 22:02:01 -0700

    created spring data jpa service for vets. closes #41

---
+++ pet-clinic-data/src/main/java/me/cheznic/learning/sfgpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,52 @@
+package me.cheznic.learning.sfgpetclinic.services.springdatajpa;
+
+import me.cheznic.learning.sfgpetclinic.model.Vet;
+import me.cheznic.learning.sfgpetclinic.repositories.VetRepository;
+import me.cheznic.learning.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Charles Nicoletti on 8/30/18
+ */
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
